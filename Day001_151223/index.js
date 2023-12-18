cb4eb4659427b6cdb9dae0d679dd942b36e6ea1c
Dev Zenu <devzenu@gmail.com>
2023-12-18 08:28:34 +0500

    Finish Day one

---
+++ Day001_151223/index.js
@@ -0,0 +1,8 @@
+console.log("hello world!")
+let y=67
+console.log(y)
+let message='asslamo alikum' ;
+console.log(message)
+y='harry';
+console.log(y)
+// humna & Humna are different variable & we can use var word as veraible .we can use dollar sign for variable we use letters but we can use start the variable from number like 8,9 etc//
